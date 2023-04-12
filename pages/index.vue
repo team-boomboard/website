1c141fb794057781b39e6c827880a330ac72ee66
James Tarpey <james@data-cubed.co.uk>
2023-04-12 13:34:35 +0100

    Setup Netlify Identity Widget

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+</script>
